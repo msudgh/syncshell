c9a629c210416bd68f9c7a01e9dec04eefc44058
Masoud <msud.ghorbani@gmail.com>
2018-05-06 22:22:39 +0430

    add config path to constants

--- syncshell/constants.py
+++ syncshell/constants.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 # Metainfo
 APP_NAME = 'syncshell'
 
@@ -6,3 +8,6 @@ LOG = {
     'level': 'INFO',
     'format': '({}) ↳ %(levelname)s - %(message)s'.format(APP_NAME)
 }
+
+# Paths
+CONFIG_PATH = '{}/.syncshell.ini'.format(Path.home())
