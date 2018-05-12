aca600678cb53a88ce296829a36bc46cd095728c
Masoud Ghorbani <msud.ghorbani@gmail.com>
2018-05-12 11:03:34 +0430

    Fix classifiers

--- setup.py
+++ setup.py
@@ -38,8 +38,9 @@ setup(
     install_requires=['pygithub', 'halo', 'fire'],
     keywords='sync shell syncshell history bash zsh linux',
     classifiers=[
-        'Development Status :: 1.0.1 - Stable',
-        'License :: MIT License',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
