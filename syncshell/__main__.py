9a6077f6967c6bcee3c2e8d73f94054748b1db7a
Masoud Ghorbani <msud.ghorbani@gmail.com>
2022-06-19 01:41:07 +0430

    tidy up main file

--- syncshell/__main__.py
+++ syncshell/__main__.py
@@ -1,13 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# syncshell.py.__main__: execute main function directly
-__author__ = "Masoud Ghorbani <msud.ghorbani@gmail.com>"
 
 from fire import Fire
-from cli import Application
-
-def main():
-    Fire(Application)
+from syncshell.cli import Application
 
 if __name__ == "__main__":
-    main()
+    Fire(Application)
