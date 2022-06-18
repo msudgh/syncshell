77f96f34a4a5f0e7d5e4273468a93b2b0bee38a9
Masoud Ghorbani <msud.ghorbani@gmail.com>
2022-06-19 01:40:47 +0430

    Improved config documentation

--- syncshell/config.py
+++ syncshell/config.py
@@ -1,33 +1,34 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# config.py: contain config class
-__author__ = "Masoud Ghorbani <msud.ghorbani@gmail.com>"
 
 import logging
 import sys
 import os
+from pathlib import Path
 from shutil import copy
-import time
 import re
-from configparser import ConfigParser
-from github import Github
-import constants, utils
+from configparser import ConfigParser, Error as ConfigParserError
+from github import Github, GithubException
+from syncshell.utils import constants, spinner as Spinner
 
 # Setup logger
-logging.basicConfig(level=constants.LOG['level'],
-                    format=constants.LOG['format'])
+logging.basicConfig(level=constants.LOG["level"], format=constants.LOG["format"])
 logger = logging.getLogger(__name__)
 
 
-class Config(object):
+class SyncShellConfig:
+    """Config class consits of fundamental methods to read, write and validate
+    config file for different scenarios of syncshell.
+    """
+
     def __init__(self, path=constants.CONFIG_PATH):
-        '''Constructor
+        """Constructor
 
         Return configuration placed in config default path
 
         Keyword arguments:
         path -- the config file path
-        '''
+        """
         self.path = path
         self.parser = ConfigParser()
         self.gist = None
@@ -40,57 +41,58 @@ class Config(object):
         return self
 
     def read(self):
-        ''' Read and parse config file and config object '''
+        """Read and parse config file and config object"""
         try:
             self.parser.read(self.path)
-            self.gist = Github(self.parser['Auth']['token'])
+            self.gist = Github(self.parser["Auth"]["token"])
 
             # Set Shell and write new config
-            if (not self.parser['Shell']['name'] or
-                    not self.parser['Shell']['path']):
+            if not self.parser["Shell"]["name"] or not self.parser["Shell"]["path"]:
                 # Extract shell name
                 regex = r"([^/]*$)"
                 matches = re.search(regex, constants.SHELL)
-                shell = matches.group() or 'zsh'
+                shell = matches.group() or "zsh"
 
-                self.parser['Shell']['name'] = shell
-                self.parser['Shell']['path'] = constants.HISTORY_PATH[shell]
+                self.parser["Shell"]["name"] = shell
+                self.parser["Shell"]["path"] = constants.HISTORY_PATH[shell]
 
                 self.write()
 
             return True
-        except:
-            logger.error('Unable to read config file.')
-
-            return False
-            sys.exit(0)
+        except ConfigParserError:
+            logger.error("Unable to read config file.")
+            sys.exit(1)
 
     def write(self, path=None):
-        ''' Set and write new config '''
+        """Set and write new config"""
         self.path = path or constants.CONFIG_PATH
 
         try:
-            with open(self.path, 'w') as file:
+            with open(self.path, "w") as file:
                 self.parser.write(file)
                 file.close()
 
             return True
-        except:
-            logger.error('Unable to set config file.')
-
-            return False
+        except IOError:
+            logger.error("Unable to set config file.")
             sys.exit(1)
 
-    # Check user exists
     def is_logged_in(self):
-        spinner = utils.Spinner('Check authenticated ...')
+        """Check user exists in config file"""
+        spinner = Spinner.NewTask("Check authenticated ...")
 
         try:
-            self.gist.get_user().login
-            success_text = 'Your Github token key authenticated and confirmed.'
-            spinner.succeed(success_text)
-            return True
-        except:
-            fail_text = 'Your Github token key is not valid. Authenticate first.'
+            if self.gist.get_user().login:
+                success_text = "Your Github token key authenticated and confirmed."
+                spinner.succeed(success_text)
+                return True
+
+            return False
+        except GithubException:
+            fail_text = "Your Github token key is not valid. Authenticate first."
             spinner.fail(fail_text)
             return False
+
+    def get_history_path(self):
+        """Return path of shell history file"""
+        return Path.joinpath(Path.home(), self.parser["Shell"]["path"])
